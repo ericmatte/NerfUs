86d8757a3adc42e831689865ffc650f43bfa20f6
Eric Matte <eric.matte@usherbrooke.ca>
2017-03-30 12:49:17 -0400

    Server config.

--- api/apache/nerfus.wsgi
+++ api/apache/nerfus.wsgi
@@ -1,9 +1,9 @@
-activate_this = '/home/ubuntu/.virtualenvs/sw-flask/bin/activate_this.py'
+activate_this = '/home/ubuntu/.virtualenvs/sw-nerfus/bin/activate_this.py'
 exec(open(activate_this).read(), dict(__file__=activate_this))
 
 import sys
 import os
-sys.path.insert(0, '/opt/flask/endless')
+sys.path.insert(0, '/opt/flask/nerfus')
 os.environ['MODE'] = 'PROD'
 
 from endless.flask import app as application
--- api/nerfus/flask.py
+++ api/nerfus/flask.py
@@ -5,7 +5,7 @@ from nerfus.server.database import init_db
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-selected_config_file = ('../../deployment_config/config_{0}.cfg'
+selected_config_file = ('../../deployment_config/nerfus/config_{0}.cfg'
                         .format('prod' if os.environ.get('MODE', '') == 'PROD' else 'debug'))
 app.config.from_pyfile(selected_config_file)
 app.secret_key = 'f3c82c6484157c7ba650b18c38837cca'
